e2bafc7bd5bacde3953bc89f6152488485786605
Lucerito <Lucerito@localhost>
2020-09-14 21:54:24 -0500

    Estructura del Proyecto

---
+++ appRentaAutosEJB/ejbModule/renta/model/manager/ManagerRenta.java
@@ -0,0 +1,20 @@
+package renta.model.manager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerRenta
+ */
+@Stateless
+@LocalBean
+public class ManagerRenta {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerRenta() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ appRentaAutosWeb/src/renta/model/controller/BeanRenta.java
@@ -0,0 +1,17 @@
+package renta.model.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class BeanRenta implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public BeanRenta() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
